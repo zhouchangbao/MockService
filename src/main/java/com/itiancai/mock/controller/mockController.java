d5b20d3aceb0a831d0512ff66f48a4ee63c03edf
潘森 <pansen@pansendeMacBook-Pro.local>
2015-06-12 18:25:09 +0800

    实现实名、投标、开通协议

---
+++ src/main/java/com/itiancai/mock/controller/mockController.java
@@ -0,0 +1,57 @@
+package com.itiancai.mock.controller;
+
+import com.google.gson.Gson;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by pansen on 15/6/5.
+ */
+@Controller
+@RequestMapping("/spay/pay")
+public class mockController {
+
+  Logger logger=Logger.getLogger(this.getClass().getName());
+  @ResponseBody
+  @RequestMapping(value = "payservice",produces = "text/html")
+  public String mockService(@RequestParam("service") String service, HttpServletRequest request) {
+    String str = "";
+    //实名认证mock
+    if ("mer_register_person".equals(service)) {
+      str = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n"
+            + "<html>\n"
+            + "  <head>\n"
+            + "<META NAME=\"MobilePayPlatform\" CONTENT=\"account_id="+RandomStringUtils.randomNumeric(14)+"&mer_id=7099079&reg_date=20150602&ret_code=0000&ret_msg=交易成功。&sign_type=RSA&user_id=UB"+RandomStringUtils.randomNumeric(30)+"&version=1.0&sign=BF7kfv3+d1l0wh3/hhJpS16pDgEhVLUzYxkzkmu25T92mbiM0yEyFWJWHV+YmodcNsiYKGPDC19HVBENf2rxdyReUPOrecKkAM+f9lh24mv7HapMhJl/48XvDoTR9dy/JMAOsvaZ+lAsX7GF4is7q9WWAffSh1FkXjSwyRbnbT8=\">\n"
+            + "  </head>\n"
+            + "  <body>\n"
+            + "  </body>\n"
+            + "</html>";
+    }
+    //投标mock
+    if ("project_transfer_nopwd".equals(service)) {
+      str = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n"
+            + "<html>\n"
+            + "  <head>\n"
+            + "<META NAME=\"MobilePayPlatform\" CONTENT=\"mer_check_date=20150604&mer_date=20150604&mer_id=7099079&order_id=16103813557010951515&ret_code=0000&ret_msg=交易成功。&sign_type=RSA&trade_no=1506045029435135&version=1.0&sign=gabAVsQKP0osJDl4tm0EBhMkVTANwu6ELn4o96E5fmXd8LPm3EaIUpGTc6Xss8UmVzpzs7e59S2EhMDx1ucAVnH+2L8Pz8KdPynIgY173g4VsplhtAQzXxCrHlFk6a6K+LXqWr1wshUzoSzQ1kUhWXifHmDyHpB1YflLuPBLTh0=\">\n"
+            + "  </head>\n"
+            + "  <body>\n"
+            + "  </body>\n"
+            + "</html>";
+    }
+    //开通协议
+    //开通投资协议http://localhost:8080/api/v2/upayment/bindAgreementReturn?ret_code=0000&user_id=UB201501221442450000000002741552&user_bind_agreement_list=ZTBB0G00,0000
+    //开通划款协议http://localhost:8080/api/v2/upayment/bindAgreementReturn?ret_code=0000&user_id=UB201501221442450000000002741552&user_bind_agreement_list=ZHKB0H01,0000
+    logger.info(service);
+    logger.info(str);
+    return str;
+
+  }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,44 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: pansen
+  Date: 15/6/12
+  Time: 下午3:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+    <script type="text/javascript" src="js/jquery-1.11.3.js"></script>
+    <script>
+        function creatAgreement() {
+            var UMPAccount = $('#UMPAccount').val();
+            var agreementType=$('#agreementType').val();
+            window.location.href =
+                    "http://localhost:8080/api/v2/upayment/bindAgreementReturn?ret_code=0000&user_id="
+                    + UMPAccount + "&user_bind_agreement_list="+agreementType;
+        }
+    </script>
+</head>
+<body>
+<table align="center">
+    <tr align="center">
+        <td align="center">开通UMP协议</td>
+    </tr>
+    <tr align="center">
+        <td align="center">协议类型:
+            <select id="agreementType">
+                <option value="ZTBB0G00,0000">投资</option>
+                <option value="ZTBB0G01,0000">还款</option>
+            </select>
+        </td>
+    </tr>
+    <tr align="center">
+        <td align="center">UMP账号:<input type="text" id="UMPAccount"/></td>
+    </tr>
+    <tr align="center">
+        <td align="center"><input type="button" onclick="creatAgreement()" value="提交"/></td>
+    </tr>
+</table>
+</body>
+</html>
